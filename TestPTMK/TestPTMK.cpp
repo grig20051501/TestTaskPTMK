9df0eccaff73ff008e9626dbd78163559a645619
Scuritel <grisha_pitsuha@mail.ru>
2024-11-04 02:06:48 +0300

    feat: add basic SQLite

--- TestPTMK/TestPTMK.cpp
+++ TestPTMK/TestPTMK.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
+#include <sqlite3.h>
 #include "Employee.h"
 
 using namespace std;
 
 int main() {
+	sqlite3* DB;
+	bool ex = false;
+	sqlite3_open("DataBase.db", &DB);
+
+	if (ex) { cout << "Error"; }
+	else { cout << "Succesfull"; }
 	Employee a1("grisha", "2005-01-15", true);
 	cout << "Hello" << endl;
 
 	a1.printInfo();
 	cout << a1.getAge();
+
+	sqlite3_close(DB);
 
 	return 0;
 }
